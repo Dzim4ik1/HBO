2ac689b1d2b76013dc15b0b26502c66c46ec29d4
Dzim4ik1 <vozhzhov.dmitriy@gmail.com>
2023-03-12 18:24:28 +0200

    added swiper && subscriptions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-console.log('script run');
+const swiper = new Swiper('.swiper', {
+   
+    slidesPerView: 6,
+   
+  
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
