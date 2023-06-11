6cd0fb081114162b6696a8fbf2777e862141abb6
Dzim4ik1 <vozhzhov.dmitriy@gmail.com>
2023-06-11 13:03:57 +0300

    finish modal

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
-let checkbox = document.querySelector('.custom-checkbox');
+//// custom checkbox
+//let checkbox = document.querySelector('.custom-checkbox');
 
+//checkbox.addEventListener('click', function() {
+//    if (checkbox.classList.contains('checked')){
+//        checkbox.classList.remove('checked') 
+//    } else {
+//        checkbox.classList.add('checked') 
+//    }
+//})
 
-checkbox.addEventListener('click', function() {
-    if (checkbox.classList.contains('checked')){
-        checkbox.classList.remove('checked') 
-    } else {
-        checkbox.classList.add('checked') 
-    }
+// modal 
+let closeModal = document.querySelector('.close-modal');
+let modal = document.querySelector('.modal');
+let signUp = document.querySelector('.sign_btn');
+
+signUp?.addEventListener('click', (e)=> {
+	e.preventDefault;
+	modal.style.visibility = 'visible'
+
 })
+
+closeModal?.addEventListener('click', (e)=> {
+	modal.style.visibility = 'hidden'
+})
+
 
